114853ea87da1de73d6654179c67e1a58f4ef1d3
muhsinachipra <muhsinachipra@gmail.com>
2024-03-25 10:26:28 +0530

    React Router Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
-import HomeScreen from './screens/HomeScreen'
+import { Outlet } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
       <Header />
-      <HomeScreen />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
   )
 }
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { Container, Card, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Hero = () => {
     return (
@@ -12,12 +13,16 @@ const Hero = () => {
                         Bootstrap library
                     </p>
                     <div className='d-flex'>
-                        <Button variant='primary' href='/login' className='me-3'>
-                            Sign In
-                        </Button>
-                        <Button variant='secondary' href='/register'>
-                            Sign Up
-                        </Button>
+                        <LinkContainer to='/login'>
+                            <Button variant='primary' className='me-3'>
+                                Sign In
+                            </Button>
+                        </LinkContainer>
+                        <LinkContainer to='/register'>
+                            <Button variant='secondary'>
+                                Sign Up
+                            </Button>
+                        </LinkContainer>
                     </div>
                 </Card>
             </Container>
