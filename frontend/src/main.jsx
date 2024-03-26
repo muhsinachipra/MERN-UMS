dba097cd9d6bfa31d9a299b866c81e4cd9ca086a
muhsinachipra <muhsinachipra@gmail.com>
2024-03-26 10:29:36 +0530

    Auth Slice & API Slices implemented

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom'
+import store from './store.js'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
@@ -24,7 +26,9 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Provider store={store} >
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider >
 )
